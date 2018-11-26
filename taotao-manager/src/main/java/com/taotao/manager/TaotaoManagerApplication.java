3eea55e4968f54a8c21f5595436f486d206497e4
minatoyukina <1096445518@qq.com>
2018-11-26 23:01:21 +0800

    common package

---
+++ taotao-manager/src/main/java/com/taotao/manager/TaotaoManagerApplication.java
@@ -0,0 +1,12 @@
+package com.taotao.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoManagerApplication.class, args);
+    }
+}
