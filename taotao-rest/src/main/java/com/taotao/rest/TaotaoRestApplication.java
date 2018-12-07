96da180df52a081dd76f29ab35dbb2ffa21fc04b
minatoyukina <1096445518@qq.com>
2018-12-07 10:58:56 +0800

    restful

---
+++ taotao-rest/src/main/java/com/taotao/rest/TaotaoRestApplication.java
@@ -0,0 +1,12 @@
+package com.taotao.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoRestApplication.class, args);
+    }
+}
